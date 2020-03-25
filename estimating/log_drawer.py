2ff22356e402cc78ec2392bd1a6a93ca4811c324
asari-saminun <saminun02@gmail.com>
2020-03-25 20:35:30 +0900

    first version of estimator

---
+++ estimating/log_drawer.py
@@ -0,0 +1,99 @@
+import os
+import json
+import argparse
+import yaml
+import math
+
+import numpy as np
+from matplotlib import pyplot
+
+# 設定ファイル
+CONFIG_FILE = "config.yaml"
+# 学習済みモデルの格納先
+MODEL_DIR = "result"
+
+def parse_log(log_dir, log_info):
+    # ログの解析
+    epochs = []
+    train_loss = []
+    train_acc = []
+    val_loss = []
+    val_acc = []
+    val_rough_acc = []
+
+    for idx in range(len(log_info)):
+        epochs.append(log_info[idx]["epoch"])
+        train_loss.append(log_info[idx]["train/loss"])
+        train_acc.append(log_info[idx]["train/acc"])
+        val_loss.append(log_info[idx]["val/loss"])
+        val_acc.append(log_info[idx]["val/acc"])
+        val_rough_acc.append(log_info[idx]["val/rough_acc"])
+
+    # lossやaccの最大最小を表示
+    print("[train/loss] min={0:.6f} ({1:3d}epoch)".format(min(train_loss), train_loss.index(min(train_loss))+1))
+    print("[train/ acc] max={0:.6f} ({1:3d}epoch)".format(max(train_acc), train_acc.index(max(train_acc))+1))
+    print("[val  /loss] min={0:.6f} ({1:3d}epoch)".format(min(val_loss), val_loss.index(min(val_loss))+1))
+    print("[val  / acc] max={0:.6f} ({1:3d}epoch)".format(max(val_acc), val_acc.index(max(val_acc))+1))
+    print("[val  /racc] max={0:.6f} ({1:3d}epoch)".format(max(val_rough_acc), val_rough_acc.index(max(val_rough_acc))+1))
+
+    epochs_arr = np.array(epochs, dtype=np.uint16)
+    train_loss_arr = np.array(train_loss, dtype=np.float32)
+    train_acc_arr = np.array(train_acc, dtype=np.float32)
+    val_loss_arr = np.array(val_loss, dtype=np.float32)
+    val_acc_arr = np.array(val_acc, dtype=np.float32)
+    val_rough_acc_arr = np.array(val_rough_acc, dtype=np.float32)    
+    
+    # 学習ログのグラフ化
+    t_fig, t_axis1 = pyplot.subplots()
+    t_fig.suptitle("train")
+    t_axis2 = t_axis1.twinx()
+    t_axis2.spines["left"].set_color("blue")
+    t_axis2.spines["right"].set_color("orange")
+    t_axis1.plot(epochs_arr, train_loss_arr, label="loss", color="blue")
+    t_axis2.plot(epochs_arr, train_acc_arr, label="acc", color="orange")
+    t_axis1.set_xlabel("epochs")
+    t_axis1.set_ylabel("loss")
+    t_axis2.set_ylabel("acc")
+    t_axis1.set_ylim([0, math.ceil(max(train_loss + val_loss))])
+    t_axis2.set_ylim([0, 1])
+    t_handler1, t_label1 = t_axis1.get_legend_handles_labels()
+    t_handler2, t_label2 = t_axis2.get_legend_handles_labels()
+    t_axis1.legend(t_handler1 + t_handler2, t_label1 + t_label2)
+    t_fig.tight_layout()
+
+    t_out_path = os.path.join(log_dir, "train")
+    t_fig.savefig(t_out_path)
+
+    # # 検証ログのグラフ化
+    v_fig, v_axis1 = pyplot.subplots()
+    v_fig.suptitle("validate")
+    v_axis2 = v_axis1.twinx()
+    v_axis2.spines["left"].set_color("blue")
+    v_axis2.spines["right"].set_color("orange")
+    v_axis1.plot(epochs_arr, val_loss_arr, label="loss", color="blue")
+    v_axis2.plot(epochs_arr, val_acc_arr, label="acc", color="orange")
+    v_axis2.plot(epochs_arr, val_rough_acc_arr, label="rough_acc", color="red")
+    v_axis1.set_xlabel("epochs")
+    v_axis1.set_ylabel("loss")
+    v_axis2.set_ylabel("acc")
+    v_axis1.set_ylim([0, math.ceil(max(train_loss + val_loss))])
+    v_axis2.set_ylim([0, 1])
+    v_handler1, v_label1 = v_axis1.get_legend_handles_labels()
+    v_handler2, v_label2 = v_axis2.get_legend_handles_labels()
+    v_axis1.legend(v_handler1 + v_handler2, v_label1 + v_label2)
+    v_fig.tight_layout()
+
+    v_out_path = os.path.join(log_dir, "validate")
+    v_fig.savefig(v_out_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()    
+    # 設定ファイルの読み込み
+    with open(CONFIG_FILE, "r") as f:
+        config = yaml.load(f)
+
+    log_dir = os.path.join(config["result_dir"], config["test_dir"])
+    log_path = os.path.join(log_dir, MODEL_DIR, "log")
+    log_info = json.load(open(log_path, "r"))
+
+    parse_log(log_dir, log_info)  
---
+++ estimating/train.py
@@ -0,0 +1,126 @@
+import os
+import sys
+import glob
+import re
+import yaml
+import datetime
+import shutil
+
+import numpy as np
+import chainer
+from chainer.backends import cuda
+from chainer.optimizers import Adam
+from chainer.datasets import TupleDataset
+from chainer.iterators import SerialIterator
+from chainer.training import Trainer
+from chainer.training import extensions
+from chainer.serializers import load_npz
+
+from lib.model import Estimator
+from lib.updater import EstimatorUpdater
+from lib.validator import Validator
+
+# 設定ファイル
+CONFIG_FILE = "config.yaml"
+# 学習済みモデルの出力先
+MODEL_DIR = "result"
+# 検証ログの出力先
+VALIDATE_DIR = "validate"
+
+def setup():
+    # 設定ファイルの読み込み
+    with open(CONFIG_FILE, "r") as f:
+        config = yaml.load(f)
+
+    xp = np if not config["use_gpu"] else cuda.cupy
+    
+    # 学習結果出力先の設定
+    restart = config["restart_dir"] is not None
+    if restart:
+        result_children_dir = config["restart_dir"]
+    else:
+        result_children_dir = "result_" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        
+    result_dir = os.path.join(config["result_dir"], result_children_dir)
+    result_dir_train = os.path.join(result_dir, MODEL_DIR)
+    result_dir_val = os.path.join(result_dir, VALIDATE_DIR)
+
+    # 学習データの読み込み
+    train_scores = []
+    train_score_lvs = []
+    with open(os.path.join(config["score_dir"], config["train_list"]), "r") as tr_f:
+        train_paths = list(map(lambda x: os.path.join(config["score_dir"], x.split("\n")[0]), tr_f.readlines()))
+
+    for idx, npy_path in enumerate(train_paths):
+        score = xp.load(npy_path)
+        score[:, 8] /= 100.0
+        # 譜面を流れてくる順に直して小節ごとに区切る
+        score = score[::-1].reshape((-1, 1728))
+        train_scores.append(score)
+        train_score_lvs.append(int(os.path.basename(npy_path)[2], 16) - 1)
+        sys.stdout.write("\rtrain score loaded: {0:4d}/{1}".format(idx+1, len(train_paths)))
+    sys.stdout.write("\n")
+
+    # 検証データの読み込み
+    val_scores = []
+    val_score_lvs = []
+    val_score_names = []
+    with open(os.path.join(config["score_dir"], config["validate_list"]), "r") as val_f:
+        val_paths = list(map(lambda x: os.path.join(config["score_dir"], x.split("\n")[0]), val_f.readlines()))
+
+    for idx, npy_path in enumerate(val_paths):
+        score = xp.load(npy_path)
+        score[:, 8] /= 100.0
+        # 譜面を流れてくる順に直して小節ごとに区切る
+        score = score[::-1].reshape((-1, 1728))
+        val_scores.append(score)
+        score_name = os.path.basename(npy_path)
+        val_score_lvs.append(int(score_name[2], 16) - 1)
+        val_score_names.append(score_name)
+        sys.stdout.write("\rvalidate score loaded: {0:4d}/{1}".format(idx+1, len(val_paths)))
+    sys.stdout.write("\n")
+
+    # optimizer
+    model = Estimator()
+    if xp is not np:
+        model.to_device("@cupy:0")    
+    optimizer = Adam(float(config["lr"]))
+    optimizer.setup(model)
+
+    # iterator, updater, trainer, extension
+    train_dataset = TupleDataset(train_scores, train_score_lvs)
+    train_iterator = SerialIterator(train_dataset, int(config["batch_size"]))
+    val_dataset = TupleDataset(val_scores, val_score_lvs, val_score_names)
+    val_iterator = SerialIterator(val_dataset, int(config["batch_size"]), repeat=False, shuffle=False)
+
+    updater = EstimatorUpdater(iterator=train_iterator, optimizer=optimizer)
+    trainer = Trainer(updater, stop_trigger=(config["epochs"], "epoch"), out=result_dir_train)
+
+    trainer.extend(Validator(val_iterator, result_dir_val), trigger=(1, "epoch"))
+    trainer.extend(extensions.snapshot(filename="snapshot_epoch_{.updater.epoch}"))
+    trainer.extend(extensions.LogReport(trigger=(1, "epoch")), trigger=(1, "epoch"))
+    trainer.extend(extensions.PrintReport(["epoch", "train/loss", "train/acc", "val/loss", "val/acc", "val/rough_acc"]))
+    trainer.extend(extensions.ProgressBar(update_interval=5))
+
+    if restart:
+        # 学習を再開するモデルを特定
+        snapshot_path_format = os.path.join(result_dir_train, "snapshot_epoch_*")
+        snapshots = [os.path.basename(fname) for fname in glob.glob(snapshot_path_format)]
+        if len(snapshots) == 0:
+            print("There does not exist a model to restart training.")
+            exit()
+        else:
+            pattern = re.compile("snapshot_epoch_([0-9]+)")
+            snapshot_epochs = list(map(lambda x: int(pattern.search(x).group(1)), snapshots))
+            prev_snapshot_idx = snapshot_epochs.index(max(snapshot_epochs))
+            prev_snapshot = snapshots[prev_snapshot_idx]
+            
+        load_npz(os.path.join(result_dir_train, prev_snapshot), trainer)
+
+    shutil.copy2(CONFIG_FILE, result_dir)
+
+    return trainer
+
+if __name__ == "__main__":
+    trainer = setup()
+    trainer.run()
